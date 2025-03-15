1807fcbbb9b0c8489f295026aef8b4fef81afa84
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2025-03-15 10:14:26 +0530

    deployment

--- client/src/services/authService.tsx
+++ client/src/services/authService.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const registerUser = async (email: string, password: string, role: string) => {
-  return axios.post("http://localhost:3000/users/register", { email, password, role });
+  return axios.post("https://claims-management-system-2d30.onrender.com/users/register", { email, password, role });
 };
 
 export const loginUser = async (email: string, password: string) => {
-  return axios.post("http://localhost:3000/auth/login", { email, password });
+  return axios.post("https://claims-management-system-2d30.onrender.com/auth/login", { email, password });
 };
