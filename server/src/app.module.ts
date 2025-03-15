7820a01314a6c8976f7c0aad08f3f34b2fa20786
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2025-03-15 09:59:58 +0530

    deployment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,3 +1,29 @@
+// import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { AuthModule } from './auth/auth.module';
+// import { UsersModule } from './users/users.module';
+// import { ClaimsModule } from './claims/claims.module';
+// import { CorsMiddleware } from '@nest-middlewares/cors';
+// import * as dotenv from 'dotenv';
+
+// dotenv.config();
+
+// @Module({
+//   imports: [
+//     MongooseModule.forRoot(process.env.MONGODB_URI || 'fallback'),
+//     AuthModule,
+//     UsersModule,
+//     ClaimsModule,
+//   ],
+// })
+// export class AppModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(CorsMiddleware).forRoutes('*');
+//   }
+// }
+// // export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+//   await app.listen(3000);
+//   // console.log('Server is running on http://localhost:3000');
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: '*',
+    origin: 'https://claims-management-system-client.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: true, 
   });
+
   await app.listen(3000);
-  // console.log('Server is running on http://localhost:3000');
 }
 bootstrap();
