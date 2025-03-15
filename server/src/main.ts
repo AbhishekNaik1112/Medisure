4957cb9f5df5fbc8e91317255303c466ff59f860
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2025-03-15 09:50:56 +0530

    deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://claims-management-system-client.vercel.app/',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   await app.listen(3000);
-  console.log('Server is running on http://localhost:3000');
+  // console.log('Server is running on http://localhost:3000');
 }
 bootstrap();
